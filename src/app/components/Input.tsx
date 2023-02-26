71cd202893c27ee4f6db2eca3f410bb0bae990cd
phaneendra <phaneendrapilli777@gmail.com>
2023-02-26 16:01:48 +0530

    forms fixes

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 export default function Input() {
   const [value, setValue] = useState("");
 
-  const handleSubmit = () => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     if (value.toLowerCase() !== "https://adventurequest.com/2k23l3") {
       alert("Entered link is not valid");
       return;
@@ -16,16 +17,17 @@ export default function Input() {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <input
           type="text"
           value={value}
           onChange={(e) => setValue(e.target.value)}
           placeholder="Enter text here"
+          required
           className="mt-1 w-full rounded-md border-gray-400 shadow-sm sm:text-sm md:text-base mb-6"
         />
         <button
-          onClick={handleSubmit}
+          type="submit"
           className="bg-blue-400 text-white px-6 py-2 rounded-md text-lg hover:bg-blue-500 duration-300"
         >
           Next &rarr;
